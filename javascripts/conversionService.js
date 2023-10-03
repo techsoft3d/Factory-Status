335c1aaf520b67f63c7eb3747d5faf180d873c86
Ricky Johnson <ricky.johnson@techsoft3d.com>
2023-10-03 14:34:31 -0400

    upgraded to caas

--- javascripts/conversionService.js
+++ javascripts/conversionService.js
@@ -1,40 +1,35 @@
 var factory_uids = [
-        "478f45db-a31c-4ced-88a8-0fa57febdcf2", //factory
-        "eced11eb-15e2-4942-b59f-9a423e2a8f10",//cmm assembly
-        "8235ee04-2b49-4288-ab1d-4483647b19de", //nsrobot5
-        "22cb3635-120d-49a7-a0d7-2ff0edcbb5fd",//pickuprobot1
-        "abd650c0-e04c-4c25-9d3b-f90c7f497de0",//weldrobot1
+        "ad250b7d-5fe5-4501-99a2-23fbfb7bcadd", //factory
+        "44529bbd-5a23-4d4e-be8c-301d419b9b25",//cmm assembly
+        "00e21aa8-849e-42b6-9cee-d8486ffec1ff", //nsrobot5
+        "8fe956a3-6b13-4a3a-940b-d726a76b741f",//pickuprobot1
+        "8ecabe07-8a59-40cf-92d9-c5b5be22a482",//weldrobot1
 ]
 
 
 
 export async function startViewer(model, container) {
-        const conversionServiceURI = "https://csapi.techsoft3d.com";
-
         var viewer;
-
-        let res = await fetch(conversionServiceURI + '/api/streamingSession');
-        var data = await res.json();
-        var endpointUriBeginning = 'ws://';
-
-        if(conversionServiceURI.substring(0, 5).includes("https")){
-                endpointUriBeginning = 'wss://'
-        }
-        await fetch(conversionServiceURI + '/api/enableStreamAccess/' + data.sessionid, { method: 'put', headers: { 'items': JSON.stringify(factory_uids) } });
-   
-
+        let sessioninfo = await caasClient.getStreamingSession();
+        await caasClient.enableStreamAccess(sessioninfo.sessionid, factory_uids);
+        
         viewer = new Communicator.WebViewer({
                 containerId: container,
-                endpointUri: endpointUriBeginning + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
+                endpointUri: sessioninfo.endpointUri,
                 model: model,
                 boundingPreviewMode: "none",
                 enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer",
                 rendererType: 0
         });
 
-        // viewer.start();
 
         return viewer;
 
 }
 
+async function fetchVersionNumber() {
+        let data = await caasClient.getHCVersion();
+        versionNumer = data;        
+        return data
+      }
+      
